ab8ffe32d41e69a0c05fb803362141af987696e0
Patrin <patrin.sinteppadon@gmail.com>
2021-05-04 21:41:23 -0700

    add about-page feature

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import contactPlaceholder from '../img/contact-card-placeholder.png';
+import './about.css';
 
 function About() {
   return(
     <div className="about-page">
-      ABOUT
+      <section className="overview">
+        <div>
+          <h1>
+            The Project
+          </h1>
+          <p>
+            This is a University of Washington Information School Capstone project for Winter/Spring 2021. 
+          </p>
+          <p>The backend of the application is inactive.</p>
+          <p>The project will shut down May 26th.</p>
+        </div>
+      </section>
+      <hr style={{ width: '20%', borderBottom: '2px solid #4A69D9' }}/>
+      <section className="contact-us" style={{ marginTop: '50px' }}>
+      <div>
+          <h1>
+            Contact Us
+          </h1>
+          <div>
+            <img 
+              style={{ width: '25%' }}
+              src={contactPlaceholder} 
+              alt="contact placeholder"
+            />
+            <img 
+              style={{ width: '25%' }}
+              src={contactPlaceholder} 
+              alt="contact placeholder"
+            />
+          </div>
+          <div>
+            <img 
+              style={{ width: '25%' }}
+              src={contactPlaceholder} 
+              alt="contact placeholder"
+            />
+            <img 
+              style={{ width: '25%' }}
+              src={contactPlaceholder} 
+              alt="contact placeholder"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
